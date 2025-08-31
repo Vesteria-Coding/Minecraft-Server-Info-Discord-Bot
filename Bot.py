7253a00d4bcfe28077873ccdac1a86c45799bb42
Holden Blair <holden.blairpeace17@gmail.com>
2025-08-31 13:28:29 -0400

    Implement Discord bot with Minecraft server commands

---
+++ Bot.py
@@ -0,0 +1,88 @@
+import os
+import base64
+import discord
+import requests
+import argparse
+import time as t
+from dotenv import load_dotenv, dotenv_values
+from discord import app_commands, Interaction, Embed, File
+
+# Setup Credentials
+load_dotenv()
+GUILD_ID = os.getenv("GUILD_ID")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MINECRAFT_SERVER_IP = os.getenv("MINECRAFT_SERVER_IP")
+
+# Setup
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    print(f"Bot is ready. Logged in as {client.user} (ID: {client.user.id})")
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
+
+# Commands
+@tree.command(name="ping", description="sends ping of bot", guild=discord.Object(id=GUILD_ID))
+async def ping(interaction: discord.Interaction):
+    latency = client.latency * 1000  # Convert to ms
+    await interaction.response.send_message(f'Pong! `{latency:.2f}ms`', ephemeral=True)
+
+@tree.command(name="info", description="sends server info", guild=discord.Object(id=GUILD_ID))
+async def ping(interaction: discord.Interaction):
+    await interaction.response.defer()
+    try:
+        url = f"https://api.mcsrvstat.us/2/{MINECRAFT_SERVER_IP}"
+        response = requests.get(url)
+        data = response.json()
+    except Exception as e:
+        print(f"An Error Has Occurred: {e}")
+
+    # Server info
+    version = data.get("version")
+    online = data.get("online")
+
+    # Players
+    players_online = data.get("players", {}).get("online", 0)
+    players_max = data.get("players", {}).get("max", 0)
+    player_list = data.get("players", {}).get("list", [])
+
+    # Download and decord thumbnail
+    base64_string = data["icon"].split(",")[1]
+    with open("server_icon.png", "wb") as image_file:
+        image_file.write(base64.b64decode(base64_string))
+
+    # Check to see if player list is too BIG
+    total_charcters = 0
+    for player in player_list:
+        for charcter in player:
+            total_charcters += 1
+
+    async def info(interaction: discord.Interaction):
+        if online is True:
+            embed = discord.Embed(title="Server Info", color=discord.Color.dark_green())
+        elif online is False:
+            embed = discord.Embed(title="Server Info", color=discord.Color.dark_red())
+        else:
+            embed = discord.Embed(title="Server Info", color=discord.Color.light_gray())
+        embed.set_thumbnail(url="attachment://server_icon.png")
+        embed.add_field(name="Players", value=f"{players_online}/{players_max}", inline=False)
+        embed_charcters = (len(embed) * 1.15 + total_charcters)
+        if embed_charcters < 2000:
+            embed.add_field(name="Player List:", value=f"{', '.join(player_list)}", inline=False)
+        else:
+            print('error') # Add the list of players untill it is near 2000 charcter plus at the end +X about of players left over
+
+        if online is True:
+            embed.set_footer(text="Online 🟢")
+        elif online is False:
+            embed.set_footer(text="Offline 🔴")
+        else:
+            embed.set_footer(text="Error Getting Status")
+        file = File("server_icon.png", filename="server_icon.png")
+        await interaction.followup.send(embed=embed, file=file)
+    await info(interaction)
+    os.remove('server_icon.png')
+
+client.run(BOT_TOKEN)
